0fc240ccdfecb480fcb5f0371c871a7c193c3355
Khaled Mohamed <46958133+xElkomy@users.noreply.github.com>
2024-03-14 05:50:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-  $file = '/tmp/sample-app.log';
-  $message = file_get_contents('php://input');
-  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
-}
-else
-{
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -28,6 +18,3 @@ else
     <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
 </body>
 </html>
-<? 
-} 
-?>
